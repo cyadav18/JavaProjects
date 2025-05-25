54cc94c815a05e4d73dca0fe7d7608f3ca8f6427
cyadav18 <your-email@domain.com>
2025-05-25 14:04:35 +0530

    update time stamp and logs

--- TaskManager/login-service/src/main/java/org/guidewire/login/config/JacksonConfig.java
+++ TaskManager/login-service/src/main/java/org/guidewire/login/config/JacksonConfig.java
@@ -1,4 +1,4 @@
-package org.guidewire.taskmanager.config;
+package org.guidewire.login.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
